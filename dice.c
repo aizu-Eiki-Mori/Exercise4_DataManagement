3c300d0cee566c3760fffd68861aaa5a9b114e8d
s1260208 <s1260208@std6dc21.u-aizu.ac.jp>
2019-07-10 21:33:27 +0900

    Changed from You to name

--- dice.c
+++ dice.c
@@ -15,7 +15,7 @@ int main(){
     printf("Die %d: %d\n", i+1, a[i]);
   }
   printf("Total value: %d\n", sum);
-  if(sum > 7) printf("You won\n");
-  else printf("You lost\n");
+  if(sum > 7) printf("%s won\n", name);
+  else printf("%s lost\n", name);
   return 0;
 }
