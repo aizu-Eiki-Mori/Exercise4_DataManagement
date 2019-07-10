9002e9b50cc97557e964f4fb7e41bc90817ab89d
s1260208 <s1260208@std6dc21.u-aizu.ac.jp>
2019-07-10 21:19:35 +0900

    Initial commit

---
+++ dice.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+  int i, a[2], sum=0;
+  srand((unsigned)time(NULL));
+  printf("Rolling the dice...\n");
+  for(i = 0; i < 2; i++){
+    a[i] = rand() % 6 + 1;
+    sum += a[i];
+    printf("Die %d: %d\n", i+1, a[i]);
+  }
+  printf("Total value: %d\n", sum);
+  return 0;
+}
